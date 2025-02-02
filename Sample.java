f8c740da9d9258d6846784b64866f6889bab549f
Imanchana Kaveeshwara Bandara <bandaragamik.22@uom.lk>
2025-02-02 21:22:37 +0530

    Initial commit

---
+++ Sample.java
@@ -0,0 +1,18 @@
+public class Sample {
+    //primitive type variables
+    byte b = 10;
+    int i = 20;
+    char ch = 'a';
+
+    //array type variable
+    double[] d = {1,2,3,4,5};
+
+    //string type variable
+    String str = "Imanchana";
+
+    public static void main(String[] args){
+        Sample s = new Sample();
+        System.out.println(s.str);
+    }
+
+}
